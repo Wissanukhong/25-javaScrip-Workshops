f3d4dd5fc7dca7fd1863df7be8c9ff2270fed5af
Wissanukhong <wissanu.khong@gmail.com>
2020-03-05 08:03:22 +0700

    superholder workshop

---
+++ README.md
@@ -0,0 +1,4 @@
+### 25 javaSCript Workshops
+สำหรับ 25 javaScript จะเป็นการเรียนรู้และการฝึกฝน workshop ทั้งหมดจำนวน 25 workshops
+- Show, Hide pasword
+- superPlaceholder
---
+++ superPlaceholder/app.js
@@ -0,0 +1,15 @@
+superplaceholder({
+    el: document.getElementById('emial'),
+    sentences: [ 'Place your email here'],
+    options:{
+      loop:true
+    }
+});
+
+superplaceholder({
+    el: document.getElementById('password'),
+    sentences: ["Place your password here"],
+    options: {
+        loop: true
+    }
+});
