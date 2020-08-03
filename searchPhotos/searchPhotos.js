2e8dc322f1cfb614f1043678dbd4bc51ce79bbef
Wissanukhong <wissanu.khong@gmail.com>
2020-08-03 22:01:29 +0700

    search and change photo

--- searchPhotos/searchPhotos.js
+++ searchPhotos/searchPhotos.js
@@ -1,21 +1,42 @@
+// key search
+function keywordSearch(keyword){
+    const stockPhoto = document.createElement('img')
+    stockPhoto.src = `asset/photo-${keyword}.jpg`;
+
+    const displayPhoto = document.querySelector('.photos')
+    displayPhoto.appendChild(stockPhoto);
+}
+
 // function remove photo
 function removePhotos() {
     const photosElment = document.querySelector('.photos');
     photosElment.innerHTML= '';
 }
 
+// function show error
+function notMatching() {
+    const noPhotos = document.createElement('img');
+    noPhotos.src= `./asset/error404.png`;
+}
+
 // function search
 function searchPhotos(event) {
-    const stockPhoto = document.createElement('img')
-    stockPhoto.src = "asset/photo-1.jpg"
+    const keyword = event.target.value;
 
-    const displayPhoto = document.querySelector('.photos')
-    displayPhoto.appendChild(stockPhoto);
+    if(event.key === 'Enter' && keyword) {
+        removePhotos();
+        keywordSearch(keyword);
+    } else{
+        notMatching(); // Error 
+    }
+
+
 }
 
 // function run
 function run() {
-    searchPhotos();
+    const inputEle =document.querySelector('input');
+    inputEle.addEventListener('keydown',searchPhotos)
 }
 
 run();
