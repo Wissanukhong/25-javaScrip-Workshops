b2b5cb18347db4c06fe6cb581f6ffae6aa23c382
Wissanu.khong <wissanu.khong@gmail.com>
2020-08-30 23:14:55 +0700

    number to string method

---
+++ problem/numberMethod.js
@@ -0,0 +1,13 @@
+let myNumber = 123;
+let newNumber = myNumber.toString();
+
+let otherNumber = 456;
+otherNumber.toString();
+
+let lotsOfnumber = (100 + 23 + 5);
+lotsOfnumber.toString();
+
+console.log(typeof newNumber);
+console.log(typeof otherNumber);
+console.log(typeof lotsOfnumber);
+
